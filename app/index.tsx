79124242a6c36d059f2955fc13a1f25a02a60274
lawrencesumbi <guiansumbi@gmail.com>
2026-06-04 15:19:23 +0800

    supabase

--- app/index.tsx
+++ app/index.tsx
@@ -8,7 +8,7 @@ export default function Index() {
   useEffect(() => {
     const timer = setTimeout(() => {
       router.replace("/login"); // Change to your login modal route
-    }, 3000); // 3 seconds
+    }, 5000); // 3 seconds
 
     return () => clearTimeout(timer);
   }, []);
